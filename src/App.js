a2513474c338348535d382f6d50ab5eabdff9f33
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2020-06-16 19:18:51 -0700

    Finished assignment MVP, working on stretch

--- src/App.js
+++ src/App.js
@@ -20,17 +20,18 @@ function App() {
             <h2 className="home__name">Lions</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-            <div className="home__score">Home: <span>{hScore}</span></div>
+            <div className="home__score"><span>{hScore}</span></div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">Away: <span>{aScore}</span></div>
+            <div className="away__score"><span>{aScore}</span></div>
           </div>
         </div>
         <BottomRow />
       </section>
       <section className="buttons">
+        <button className="buttons" onClick={() => setHScore(0)}>Reset HomeScore</button>
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
@@ -38,9 +39,11 @@ function App() {
           <button className="homeButtons__fieldGoal" onClick={() => setHScore(hScore + 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => setAScore(aScore + 7)>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => setAScore(aScore + 3)>Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => setAScore(aScore + 7)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => setAScore(aScore + 3)}>Away Field Goal</button>
         </div>
+        <button className="buttons" onClick={() => setAScore(0)}>Reset AwayScore</button>
+
       </section>
     </div>
   );
